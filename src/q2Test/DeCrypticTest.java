c33bf1cefa60be5c2dfcf0d65ca0fd8f681de0aa
Seth <sethlinc4@gmail.com>
2022-02-02 16:17:12 -0800

    Changed test so that it ran in a loop instead of having multiple methods with the same code

--- src/q2Test/DeCrypticTest.java
+++ src/q2Test/DeCrypticTest.java
@@ -39,44 +39,28 @@ class DeCrypticTest {
     }
 
     @Test
-    public void OutputTest1(){
+    public void OutputLoopTesting(){
         //run main & get output
-        DeCryptic.main(new String[0]);
-        String output = getOutput();
-        int i = 0;
-        while(Character.isDigit(output.charAt(i)))
-            i++;
-        String outputCrypt = output.substring(0, i).trim();
-        //Test to make sure crypt is an 8 digit number
-        assertTrue(outputCrypt.length() == 8, "Crypt was not 8 digits");
-        //Get value of key
-        output = output.replaceAll("[^0-9]", "");
-        int outputKey = Integer.parseInt("" + output.charAt(8));
-        //Get Value of Encrypt
-        int outputEncrypt = Integer.parseInt(output.substring(9).trim());
-        int expectedEncrypt = Integer.parseInt(outputCrypt) / outputKey;
-        //test if the expected encrypt = actual encrypt
-        assertEquals(expectedEncrypt, outputEncrypt, String.format("The output of Encrypt is incorrect; Expected output was %d with key %d, but actual output was %d (Note that your key should be 1 digit)", expectedEncrypt, outputKey, outputEncrypt));
-    }
+        for(int j = 0; j < 10; j++) {
+            DeCryptic.main(new String[0]);
+            String output = getOutput();
+            output = output.split(System.lineSeparator())[j];
+            output = output.trim();
+            int i = 0;
+            while (Character.isDigit(output.charAt(i)))
+                i++;
+            String outputCrypt = output.substring(0, i).trim();
+            //Test to make sure crypt is an 8 digit number
+            assertTrue(outputCrypt.length() == 8, "Crypt was not 8 digits");
+            //Get value of key
+            output = output.replaceAll("[^0-9]", "");
+            int outputKey = Integer.parseInt("" + output.charAt(8));
+            //Get Value of Encrypt
+            int outputEncrypt = Integer.parseInt(output.substring(9).trim());
+            int expectedEncrypt = Integer.parseInt(outputCrypt) / outputKey;
+            //test if the expected encrypt = actual encrypt
+            assertEquals(expectedEncrypt, outputEncrypt, String.format("The output of Encrypt is incorrect: Expected output was %d with key %d, but actual output was %d (Note that your key should be 1 digit)", expectedEncrypt, outputKey, outputEncrypt));
+        }
 
-    @Test
-    public void OutputTest2(){
-        //run main & get output
-        DeCryptic.main(new String[0]);
-        String output = getOutput();
-        int i = 0;
-        while(Character.isDigit(output.charAt(i)))
-            i++;
-        String outputCrypt = output.substring(0, i).trim();
-        //Test to make sure crypt is an 8 digit number
-        assertTrue(outputCrypt.length() == 8, "Crypt was not 8 digits");
-        //Get value of key
-        output = output.replaceAll("[^0-9]", "");
-        int outputKey = Integer.parseInt("" + output.charAt(8));
-        //Get Value of Encrypt
-        int outputEncrypt = Integer.parseInt(output.substring(9).trim());
-        int expectedEncrypt = Integer.parseInt(outputCrypt) / outputKey;
-        //test if the expected encrypt = actual encrypt
-        assertEquals(expectedEncrypt, outputEncrypt, String.format("The output of Encrypt is incorrect; Expected output was %d with key %d, but actual output was %d (Note that your key should be 1 digit)", expectedEncrypt, outputKey, outputEncrypt));
     }
 }
