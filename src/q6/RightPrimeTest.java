b6200f0e3cd3dfd87f2e508d975ee08e03307fbf
Samantha H <hodge1214@gmail.com>
2022-02-02 19:13:12 -0500

    Created new branch and added Q6 RightPrime code and test

---
+++ src/q6/RightPrimeTest.java
@@ -0,0 +1,92 @@
+package q6;
+
+import org.junit.jupiter.api.*;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RightPrimeTest {
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+    //actual tests start now
+
+    @Test
+    public void notPrimeAfterOneOperationTest(){
+        provideInput("89");
+        RightPrime.main(new String[0]);
+        String actual = getOutput();
+        String expected = "Enter a number"+System.lineSeparator()+"false"+System.lineSeparator();
+
+        assertEquals(expected, actual, "Your program does not check the prime after one operation");
+    }
+    @Test
+    public void notPrimeAfterTwoOperationsTest(){
+        provideInput("1277");
+        RightPrime.main(new String[0]);
+        String actual = getOutput();
+        String expected = "Enter a number"+System.lineSeparator()+"false"+System.lineSeparator();
+
+        assertEquals(expected, actual, "Your program does not check the prime after two operations");
+    }
+    @Test
+    public void smallPrimeTest(){
+        provideInput("31");
+        RightPrime.main(new String[0]);
+        String actual = getOutput();
+        String expected = "Enter a number"+System.lineSeparator()+"true"+System.lineSeparator();
+
+        assertEquals(expected, actual, "Your program does not handle small right truncatable primes");
+    }
+
+    @Test
+    public void largePrimeTest(){
+        provideInput("59393");
+        RightPrime.main(new String[0]);
+        String actual = getOutput();
+        String expected = "Enter a number"+System.lineSeparator()+"true"+System.lineSeparator();
+
+        assertEquals(expected, actual, "Your program does not handle large right truncatable primes");
+    }
+    @Disabled("Assume all inputs are positive")
+    @Test
+    public void negativePrimeTest(){
+        provideInput("31");
+        RightPrime.main(new String[0]);
+        String actual = getOutput();
+        String expected = "Enter a number"+System.lineSeparator()+"false"+System.lineSeparator();
+
+        assertEquals(expected, actual, "Your program should not accept negative primes");
+    }
+    //now to test the method
+    @Test
+    public void methodTest(){
+        boolean actual = RightPrime.isRightPrime(797);
+        assertTrue(actual, "Your isRightPrime method does not correctly identify a right truncatable prime");
+        actual = RightPrime.isRightPrime(593);
+        assertTrue(actual, "Your isRightPrime method does not correctly identify a non-right truncatable prime");
+    }
+}
