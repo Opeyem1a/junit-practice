8856ebeb272c21301f65d3e629431685467b424f
Abhineeth Adiraju <abhineeth00@gmail.com>
2022-02-04 00:24:53 -0800

    Wrote test for Cryptic.java

---
+++ src/UsabilityTestingQuestions/Cryptic.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Cryptic {
+    public static void main(String[] args) {
+        Random gen = new Random();
+        int crypt = gen.nextInt(90000000) + 10000000;
+        int second = gen.nextInt(9) + 1;
+        int encrypt = crypt * second;
+        System.out.println(crypt + " has been encrypted into " + encrypt);
+    }
+}
