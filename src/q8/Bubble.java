dd1e01f15129f9bbf8655d10c60c5b37447a03c5
Samantha H <hodge1214@gmail.com>
2022-02-04 01:21:45 -0500

    Added Question as a comment in the answer files

--- src/q8/Bubble.java
+++ src/q8/Bubble.java
@@ -1,5 +1,10 @@
 package q8;
 
+//You are working on a software animation for an object that grows and pops.
+// Write a Java class called Bubble that has attributes called size of type double whose initial value is set by the input parameter of the constructor and isPopped whose initial value is false.
+// The class should also have an accessor and mutator method for each attribute, as well as a toString() method that summarizes the attribute values.
+// The program should also have a test class called TestPopping that creates an object of Bubble, grow it to twice its original size, pop it, and display the object's details.
+
 public class Bubble {
     private double size;
     private boolean isPopped;
